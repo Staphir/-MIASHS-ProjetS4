b41993f22316953fbec7de66c8c33a63ca8ed8bb
Hast0n <37366420+Hast0n@users.noreply.github.com>
2018-03-22 15:07:41 +0100

    Add files via upload

---
+++ Projet CSWD/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang=fr>
+	
+	<head>
+		<meta charset="utf-8" />
+		<title>
+			Ressources pour les maths
+		</title>
+	</head>
+	
+	<body>
+		<h1 id="hautdepage">Ressources pédagogiques pour les maths</h1>
+		<img src="images/symba.png" alt="Crochet X" width=209 heigth=200>
+		<ul >
+			<li>Acueil</li>
+			<li><a href=apropos.html>A propos</a></li>
+			<li>Ressources
+				<ul>
+				<li><a href=addition.html>Addition</a></li>
+				<li><a href=multiplication.html>Multiplication</a></li>
+				<li><a href=fractions.html>Fractions</a></li>
+				</ul>
+			</li>
+			<li><a href=contact.html>Contact</a></li>
+		</ul>
+
+		<h2>Introduction</h2>
+
+		<p><em>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.</em></p>
+		<p><em>Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula. Pellentesque rhoncus nunc et augue. Integer id felis. Curabitur aliquet pellentesque diam. Integer quis metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi vel erat non mauris convallis vehicula. Nulla et sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis vitae erat. Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna.</em></p>
+		<p><em>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus, felis magna fermentum augue, et ultricies lacus lorem varius purus. Curabitur eu amet.</em></p>
+		<p>L'utilisation de faux texte a plusieurs avantages :</p>
+		<ul>
+			<li>Le faux texte "Lorem Ipsum" a pour avantage d'utiliser des mots de longueur variable, essayant de simuler une occupation normale.</li>
+			<li>Le faux latin traditionnel a l'avantage d'être neutre.</li>
+			<li>Par rapport à du vrai texte de substitution, le faux-texte permet de s'apercevoir rapidement qu'il n'est pas définitif.</li>
+		</ul>
+
+		<h2>Liens utiles</h2>
+			<a href=https://fr.wikipedia.org/>Wikipedia</a>
+			<br/>
+			<a href=https://fr.wikipedia.org/wiki/Faux-texte>Lorem Ipsum sur Wikipedia</a>
+
+		<h2>Contact</h2>
+			<a href=contact.html>Lien vers la page contacts</a>
+		<br/>
+		<br/>
+		<a href="#hautdepage">Revenir en haut</a>
+		<br/>
+		<a href="https://www.u-bordeaux.fr/"><img src="../ressources/images/ub-logo_fr.svg" alt="Logo Université de Bordeaux" width=200></a>
+	</body>
+	
+</html>
