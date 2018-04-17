46238e1af28f6aea7e1152c11ae0b9c57261e98d
Hast0n <devreese.martin@gmail.com>
2018-04-17 12:07:57 +0200

    refonte de my_stories.php

--- Projet CSWD/story_handling/my_stories.php
+++ Projet CSWD/story_handling/my_stories.php
@@ -6,22 +6,38 @@ $menu["title"] = "Mes histoires";
 $dir1 = "../"; $dir2 = "../";
 include("../main_header.php");
 
-$requete="SELECT story.id,title FROM story INNER JOIN user ON story.User_id = user.id WHERE user.username = ?";
+$requete="SELECT * FROM story WHERE user_id = ?";
 $reponse=$pdo->prepare($requete);
-$reponse->execute(array($login_session));
-$array_stories = $reponse->fetchAll();
+$reponse->execute(array($_SESSION["user_id"]));
+$array_stories = $reponse->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <section>
     <article class="card">
         <div>
             <h2>Vos histoires</h2><hr>
-            <ul style="list-style-type:circle">
-                <?php
-                for($i=0; $i<count($array_stories); $i++){
-                    echo "<li class='mystories'><a href='../story_handling/story_display.php?story_id=".$array_stories[$i][0]."&&story_title=".htmlspecialchars($array_stories[$i][1], ENT_QUOTES)."'>".$array_stories[$i][1]."</a></li>";
-                }
-                ?>
-            </ul>
+            <table id="customers">
+            <tr>
+                <th>Titre</th>
+                <th>Date de création</th>
+                <th>Publiée</th>
+                <th>Date de publication</th>
+                <th>Likes</th>
+                <th>Dernière modification</th>
+            </tr>
+            <?php
+            for ($i=0; $i<count($array_stories); $i++) {
+                $createdon = date('M j Y g:i A', strtotime($array_stories[$i]["createdon"]));
+                $publishedon = date('M j Y g:i A', strtotime($array_stories[$i]["publishedon"]));
+                $lastmodifiedon = date('M j Y g:i A', strtotime($array_stories[$i]["lastmodifiedon"]));
+                echo "<tr onclick=window.location='story_display.php?story_id=".$array_stories[$i]["id"]."'>";
+                echo "<td>".$array_stories[$i]["title"]."</td>";
+                echo "<td>".$createdon."</td>";
+                echo "<td>".($array_stories[$i]["published"]?"Oui":"Non")."</td>";
+                echo "<td>".$publishedon."</td>";
+                echo "<td>".$array_stories[$i]["likes"]."</td>";
+                echo "<td>".$lastmodifiedon."</td></tr>";
+            } ?>
+        </table>
         </div>
     </article>
 </section>
--- Projet CSWD/story_handling/story_display.php
+++ Projet CSWD/story_handling/story_display.php
@@ -1,7 +1,7 @@
 <?php
-require_once("../user_handling/config.php");
-require_once("../user_handling/session.php");
-// require_once("../connect_database.php");
+$menu["title"] = "Mes histoires";
+$dir1 = "../"; $dir2 = "../";
+include("../main_header.php");
 
 // ATTENTION :
 // Je pense retirer le $_GET["story_title"] et uniquement se servir de l'id
@@ -15,42 +15,38 @@ require_once("../user_handling/session.php");
 // Et j'ai remplacé le secondary_header.php par le main_header.php
 // C'est mieux
 
-if(isset($_GET["story_id"]) && isset($_GET["story_title"])) {
-    $Id_story_choosed = $_GET["story_id"];
+if(!isset($_GET["story_id"]) or empty($_GET["story_id"])) {
+    header("location: my_stories.php");
     // $Title_story_choosed = $_GET["story_title"];
 } else {
-    header("location: my_stories.php");
-}
-// print_r($_SESSION);
-$query="SELECT * FROM story INNER JOIN user ON story.user_id = user.id WHERE story.id = ? AND user.id = ? ";
-$result=$pdo->prepare($query);
-$result->execute(array($Id_story_choosed, $_SESSION["user_id"]));
-$row = $result->fetchall(PDO::FETCH_ASSOC);
+    $Id_story_choosed = $_GET["story_id"];
 
-if (empty($row)) {
-    header("location: my_stories.php");
-} else {
-    $menu["title"] = "Mes histoires";
-    $dir1 = "../"; $dir2 = "../";
-    include("../main_header.php");
+    // print_r($_SESSION);
+    $query="SELECT * FROM story INNER JOIN user ON story.user_id = user.id WHERE story.id = ? AND user.id = ? ";
+    $result=$pdo->prepare($query);
+    $result->execute(array($Id_story_choosed, $_SESSION["user_id"]));
+    $row = $result->fetchall(PDO::FETCH_ASSOC);
 
-    $query_first_step="SELECT * FROM step LEFT JOIN story ON step.id_story = story.id WHERE story.id = ? AND step.id_choice = 0 ";
-    $response_first_step=$pdo->prepare($query_first_step);
-    $response_first_step->execute(array($Id_story_choosed));
-    $first_step = $response_first_step->fetchall(PDO::FETCH_ASSOC);
-    // print_r($first_step);
+    if (empty($row)) {
+        header("location: my_stories.php");
+    } else {
+        $query_first_step="SELECT * FROM step LEFT JOIN story ON step.id_story = story.id WHERE story.id = ? AND step.id_choice = 0 ";
+        $response_first_step=$pdo->prepare($query_first_step);
+        $response_first_step->execute(array($Id_story_choosed));
+        $first_step = $response_first_step->fetchall(PDO::FETCH_ASSOC);
+        // print_r($first_step);
 ?>
 <section>
     <article class="card">
         <div>
             <?php echo "<h2 style='text_align:center;'>".$row[0]["title"]."</h2><hr>";
 
-        if($first_step == NULL){
-        ?>
-        <form action="create_step.php" method="post">
-            <input type="hidden" name="id_story" value="<?php echo $Id_story_choosed ?>">
-            <input type="submit" name="new_step" value="Nouvelle étape">
-        </form>
+            if($first_step == NULL){
+            ?>
+            <form action="create_step.php" method="post">
+                <input type="hidden" name="id_story" value="<?php echo $Id_story_choosed ?>">
+                <input type="submit" name="new_step" value="Nouvelle étape">
+            </form>
     <?php } ?>
         </div>
     </article>
@@ -58,5 +54,6 @@ if (empty($row)) {
 
     <?php
     include("../footer.php");
+}
 }
 ?>
